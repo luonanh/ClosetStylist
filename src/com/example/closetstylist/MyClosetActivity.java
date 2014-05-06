44a3450bdb5c7dfa41dfa2ead6093970654f29e4
Anh Luong Tilapia <luonanh@gmail.com>
2014-05-06 08:13:55 -0500

    Add 2 buttons to MyCloset activity - AddItem and Back.

--- src/com/example/closetstylist/MyClosetActivity.java
+++ src/com/example/closetstylist/MyClosetActivity.java
@@ -1,7 +1,11 @@
 package com.example.closetstylist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MyClosetActivity extends Activity {
@@ -12,11 +16,29 @@ public class MyClosetActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.my_closet);
-		/*
-		 *         ListView listView = (ListView) findViewById(R.id.times_list);
-        timeTrackerAdapter = new TimeTrackerAdapter(this,databaseHelper.getAllTimeRecords());
-        listView.setAdapter(timeTrackerAdapter);
-		 */
+
+		// AddItem button
+		Button addItemBtn = (Button) findViewById(R.id.my_closet_btn_add_item);
+		addItemBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i1 = new Intent(MyClosetActivity.this, AddItemActivity.class);
+				startActivity(i1);
+			}
+		});
+		
+		// Back button
+		Button backBtn = (Button) findViewById(R.id.my_closet_btn_back);
+		backBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+		// Setup CursorAdapter
 		itemDatabaseHelper = new ItemDatabaseHelper(this);
 		ListView listView = (ListView) findViewById(R.id.my_closet_list);
 		itemDataAdapter = new ItemDataAdapter(this, itemDatabaseHelper.getAllTimeRecords());
